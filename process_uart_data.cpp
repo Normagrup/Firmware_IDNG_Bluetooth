50e9028f94ed28700ff197922a85b2a2cd35c6c1
mposada00 <m.posada@normagrup.com>
2025-12-12 11:05:07 +0100

    Revert "Comentar la parte de añadir devkey en inyect_node"
    This reverts commit 8fff10a9a3cdc2b43b2d678b02e21a9b6704eb98.

--- process_uart_data.cpp
+++ process_uart_data.cpp
@@ -918,13 +918,12 @@ void sendUartInyectNode(UartPort* _uartPort, uint16_t nodeAddress, Database* dat
     messageState = PENDING;
 
     while(actAtt < att && messageState == PENDING) {
-        //uint8_t devKey[16] = {0};
-        //QString devKeyStr = database->getDevKey(nodeAddress);
-        //convertDevKeyStringToByteArray(devKeyStr, devKey);
+        uint8_t devKey[16] = {0};
+        QString devKeyStr = database->getDevKey(nodeAddress);
+        convertDevKeyStringToByteArray(devKeyStr, devKey);
 
         QByteArray frame;
-        //unsigned char length = 21;
-        unsigned char length = 5;
+        unsigned char length = 21;
 
         frame.append(UART_HEADER);
         frame.append(length);
@@ -932,9 +931,9 @@ void sendUartInyectNode(UartPort* _uartPort, uint16_t nodeAddress, Database* dat
         frame.append(INYECT_NODE);
         frame.append((nodeAddress >> 8) & 0xFF);
         frame.append(nodeAddress & 0xFF);
-        /*for (uint8_t i = 0; i < 16; i++) {
+        for (uint8_t i = 0; i < 16; i++) {
             frame.append(devKey[i]);
-        }*/
+        }
         frame.append(UART_END);
 
         _uartPort->sendData(frame);
