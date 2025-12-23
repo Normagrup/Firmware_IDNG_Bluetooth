d13c6d73b7719c476dc79381ffe485039e4523e9
AlexRodriguezNG <alex.rodriguez@normagrup.com>
2025-12-23 12:14:17 +0100

    adaptacion tiempo

--- process_webserver_data.cpp
+++ process_webserver_data.cpp
@@ -1997,6 +1997,7 @@ void applyAutoAssignment(WebServer* webServer, UartPort* uartPort, Database* dat
 
     QList<UnassignedNode> unassignedNodes = database->getUnassignedNodes();
 
+    // Unos 10s por nodo --> 6 nodos/min
     for(UnassignedNode unassignedNode : unassignedNodes)
     {
         uint8_t installKey[16] = {0};
@@ -2014,7 +2015,7 @@ void applyAutoAssignment(WebServer* webServer, UartPort* uartPort, Database* dat
         }
 
         sendUartInstallKey(uartPort, unassignedNode.serial, unassignedNode.bluetoothAddress, installKey);
-        delay(5000);
+        delay(10000);
     }
 
     uint16_t newNextUnicastAddress = database->getMayorUnicastAddressOfUnassignedNodes();
