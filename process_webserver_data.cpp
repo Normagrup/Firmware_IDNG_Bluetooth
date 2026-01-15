b5c7b54761b9571237cee25b748a9e4ffebd9b6d
mposada00 <m.posada@normagrup.com>
2026-01-15 12:16:33 +0100

    corr delay

--- process_webserver_data.cpp
+++ process_webserver_data.cpp
@@ -1092,7 +1092,7 @@ void processWebServerData(QString data, WebServer* webServer, UartPort* uartPort
         sendConfirmStartScanSerial(webServer);
 
         sendUartScanSerial(uartPort);
-        delay(15000);
+        delay(295000);
         sendUartMicroReboot(uartPort);
         delay(3000);
         sendUartRplReset(uartPort); // para el rango de SCAN ADDRESS [0x6000 a 0x7999]
